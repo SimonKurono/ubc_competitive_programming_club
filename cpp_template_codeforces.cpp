71fa1bafded2ad82da3ae7fdd8f266919407eff2
SimonKurono <simonkurono@gmail.com>
2026-02-04 17:48:03 -0800

    Added template

---
+++ cpp_template_codeforces.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<long long>;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    // TODO: compute answer for this test case
+
+    // Example output (replace):
+    // cout << answer << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;                 // If problem has no t, delete this line and keep t=1
+    while (t--) solve();
+
+    return 0;
+}
